a4310668ab904b7dfaccc23b7c3aa7f690ccd932
cysof <cp4reallife@yahoo.com>
2024-10-15 11:51:12 +0100

    migrating database

--- build.sh
+++ build.sh
@@ -8,10 +8,14 @@ pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
+
+# Apply any outstanding database migrations
+python manage.py migrate
+
+
+# Create superuser
 if [[ $CREATE_SUPERUSER ]];
 then
   python manage.py createsuperuser --no-input
 fi
 
-# Apply any outstanding database migrations
-python manage.py migrate
