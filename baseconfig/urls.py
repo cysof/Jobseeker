0e08727abdf1a2634ccb55eecae3e870b36d0438
cysof <cp4reallife@yahoo.com>
2024-10-15 10:26:48 +0100

    fixing bad request erre

--- baseconfig/urls.py
+++ baseconfig/urls.py
@@ -29,13 +29,13 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('jobseek.urls')),
+    path('', include('jobseek.urls')),
     path('wallet/', include('wallet.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
