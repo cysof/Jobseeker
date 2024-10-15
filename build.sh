9bd628e0cce43acd47c46bb45178aef9ad2fc8f5
cysof <cp4reallife@yahoo.com>
2024-10-15 11:08:03 +0100

    supersure

--- build.sh
+++ build.sh
@@ -8,5 +8,10 @@ pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
+
 # Apply any outstanding database migrations
 python manage.py migrate
