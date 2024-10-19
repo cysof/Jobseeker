af448400117d5b69adfc1e095aacffda7b7cfef6
cysof <cp4reallife@yahoo.com>
2024-10-19 20:58:05 +0100

    fixing allowhosit

--- baseconfig/settings.py
+++ baseconfig/settings.py
@@ -16,8 +16,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
+ALLOWED_HOSTS = ['jobseeker-ewuk.onrender.com', 'localhost', '127.0.0.1']
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
